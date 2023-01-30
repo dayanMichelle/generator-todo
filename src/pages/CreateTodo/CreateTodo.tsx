6bbb7a84dda09e244d374c6fa2531fede72fcdcc
Dayan Arango <dayaara6@gmail.com>
2023-01-29 23:31:40 -0300

    Add | loading

--- src/pages/CreateTodo/CreateTodo.tsx
+++ src/pages/CreateTodo/CreateTodo.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Button } from "../../components/Button";
+import { Text } from "../../components/Text/Text";
 import { TodoList } from "../../components/TodoList/TodoList";
 import { Todo } from "../../types";
 
@@ -15,9 +16,11 @@ export const CreateTodo = () => {
       title: "todo 1",
       date: "2021-10-10",
       steps: [
-        { id: "1", text: "step 1", checked: false },
-        { id: "2", text: "step 2", checked: false },
-        { id: "3", text: "step 3", checked: false },
+        { id: "1", text: "Choose a variety of tomato plants that are suited for your region.", checked: false },
+        { id: "2", text: "Start the seeds indoors in a warm, sunny spot.", checked: false },
+        { id: "3", text: "Space the plants about 2 feet apart in a sunny spot", checked: false },
+        { id: "4", text: "Space the plants about 2 feet apart in a sunny spot", checked: false },
+        { id: "5", text: "Space the plants about 2 feet apart in a sunny spot", checked: false },
       ],
     };
     setSearchTodo(todo);
@@ -26,16 +29,24 @@ export const CreateTodo = () => {
   return (
     <div className={styles.container}>
       <div className={styles.search}>
-        <p>busqueda</p> 
-        <div><input type='text' className={styles.inputSearch} >buscar</input>
-        
-          </div>
-        
+        <Text size="20px" weight={700} text="Search a TODO" />
+        <div>
+          <input
+            placeholder="e.j: Plant a tomatoes "
+            type="text"
+            className={styles.inputSearch}
+          />
+          <button onClick={handleSearchTodo} className={styles.btnSearch}>
+            🔍{" "}
+          </button>
+        </div>
       </div>
       {searchTodo ? (
-        <TodoList steps={searchTodo.steps} handleChange={() => {}} />
+        <div className={styles.containerTask}>
+          {/* <TodoList steps={searchTodo.steps} handleChange={() => {}} /> */}
+        </div>
       ) : (
-        <div>.</div>
+        <TodoList steps={searchTodo.steps} handleChange={() => {}} />
       )}
       <div className={styles.buttonContainer}>
         <Button color="dark" text="Save" onClick={() => {}} />
