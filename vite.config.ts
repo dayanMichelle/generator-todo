caa4ccd2214759e0260265dfc3fba79677a6b42e
jmoraba_bci <jhan.mora@bci.cl>
2023-01-31 19:02:33 -0300

    Prueba alias path

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from "vite";
-import path from "path";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  },
+  plugins: [react(), tsconfigPaths()],
 });
