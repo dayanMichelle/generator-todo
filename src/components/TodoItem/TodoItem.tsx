4bf4794de152b0847a7c1c354f8dc6484cca0dbe
jmoraba_bci <jhan.mora@bci.cl>
2023-01-30 21:27:09 -0300

    Ref | ajuste

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,8 +1,6 @@
 import { TodoStep } from "@/types";
 import {
   Draggable,
-  DraggableProvided,
-  DraggableStateSnapshot,
 } from "react-beautiful-dnd";
 import styles from "./TodoItem.module.css";
 import { BsTrash } from "react-icons/bs";
@@ -24,25 +22,29 @@ export function TodoItem({
   return (
     <Draggable key={id} draggableId={id} index={position}>
       {(provided) => (
-
-
-
-         <div    className={`${styles.container} ${checked && styles.checked}`}
-         {...provided.draggableProps}
-         {...provided.dragHandleProps}
-         ref={provided.innerRef}
-         style={provided.draggableProps.style}>
-         <div className={styles.containerTask} onClick={() => handleChange(id)}>
-           <input type="checkbox" />
-           {/* Format "1. Choose a ..." */}
-           <p>{`${position + 1}. ${text}`}</p>
-         </div>
-         <button className={styles.btnDelete} onClick={() => deleteStepAtTodo(id)}>
-           <BsTrash className={styles.trash} size="20px" />
-         </button>
-       </div>
+        <div
+          className={`${styles.container} ${checked && styles.checked}`}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          style={provided.draggableProps.style}
+        >
+          <div
+            className={styles.containerTask}
+            onClick={() => handleChange(id)}
+          >
+            <input type="checkbox" />
+            {/* Format "1. Choose a ..." */}
+            <p>{`${position + 1}. ${text}`}</p>
+          </div>
+          <button
+            className={styles.btnDelete}
+            onClick={() => deleteStepAtTodo(id)}
+          >
+            <BsTrash className={styles.trash} size="20px" />
+          </button>
+        </div>
       )}
     </Draggable>
-   
   );
 }
